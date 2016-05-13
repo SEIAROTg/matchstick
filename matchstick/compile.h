e9c6e9d55f0e76fc06c00b5360725088cb84fa95
SEIAROTg <seiarotg@gmail.com>
2016-05-13 22:18:18 +0800

    compiling interface

---
+++ matchstick/compile.h
@@ -0,0 +1,11 @@
+#ifndef MATCHSTICK_COMPILE_H
+#define MATCHSTICK_COMPILE_H
+
+namespace _matchstick_compile {
+
+    using namespace _matchstick;
+
+    REGEX *compile_pattern(const MS_CHAR *pattern, const char *flags);
+
+}
+#endif //MATCHSTICK_COMPILE_H
