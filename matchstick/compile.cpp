745029dec3f49102c86475399e262773cd2dcd1e
SEIAROTg <seiarotg@gmail.com>
2016-05-13 22:21:36 +0800

    implement pick_special_match

--- matchstick/compile.cpp
+++ matchstick/compile.cpp
@@ -5,6 +5,15 @@
 
 namespace _matchstick_compile {
 
+    FSM_MATCH * const pick_special_match(MS_CHAR **cursor) {
+        FSM_MATCH *match = NULL;
+        MS_CHAR *pos;
+        if ((pos = wcsrchr(SPECIAL_MATCH, **cursor))) {
+            match = new FSM_MATCH_BASIC(SPECIAL_MATCH_TYPE[pos - SPECIAL_MATCH]);
+            ++*cursor;
+        }
+        return match;
+    }
     using namespace _matchstick;
 
 }
--- matchstick/compile_consts.h
+++ matchstick/compile_consts.h
@@ -1,3 +1,10 @@
 namespace _matchstick_compile {
 
+    const MS_CHAR *SPECIAL_MATCH = L"^$.";
+    const enum FSM_MATCH_TYPE SPECIAL_MATCH_TYPE[] = {
+            FSM_MATCH_TYPE_START,
+            FSM_MATCH_TYPE_END,
+            FSM_MATCH_TYPE_ANY,
+    };
+
 }
